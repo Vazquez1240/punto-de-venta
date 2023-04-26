2ad160f0c01de5c3e265d82ba52ea8da05cbbeb1
Martin de la torre vazquez <127373642+Vazquez1240@users.noreply.github.com>
2023-04-26 16:11:25 -0600

    quitamos la pagina web

--- aplicacion web/static/inicio.js
+++
@@ -1,8 +0,0 @@
-// JS
-const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu');
-
-menuBtn.addEventListener('click', () => {
-  menu.classList.toggle('show');
-  menuBtn.classList.toggle('show');
-});
